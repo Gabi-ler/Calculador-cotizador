69e9f2590c863cb631a7c34c5025c250b4fb01f2
Gonzalo <gtello.bcn@gmail.com>
2022-09-09 16:08:26 +0200

    propuestas de mejoras para el codigo mas limpio

--- javascript/app.js
+++ javascript/app.js
@@ -8,7 +8,6 @@ class Banco {
     }
 }
 
-
 function financiacion(num1, num2, num3, num4) {
     return num1 * (num2 + num3 * num4)
 }
@@ -28,25 +27,25 @@ let salida;
 //----------------Funcion que integra el calculo princiapl del prestamo----
 const calculador = () => {
     if (monto === 0) return //(alert('Monto invalido'))
-    if (cantCuotas != 12 && cantCuotas != 24 && cantCuotas != 36 && cantCuotas != 48) return (
-        (Swal.fire({
+    if (cantCuotas !== 12 && cantCuotas !== 24 && cantCuotas !== 36 && cantCuotas !== 48) {
+        return Swal.fire({
             title: 'Inválido',
             text: 'Seleccione el monto de cuotas',
             icon: 'warning',
             showConfirmButton: false,
             timer: 2500
-        })))
+        });
+    }
 
     interes = financiacion(monto, num, tasa, cantCuotas)
     cuota = cuotas(interes, cantCuotas)
 
-    const impCalculados = new Banco(monto, cantCuotas, interes, cuota)
-    return impCalculados
+    return new Banco(monto, cantCuotas, interes, cuota)
 }
 
 //---------------Trayendo template del HTML
 let resultado = []
-const resultadoLS = JSON.parse(localStorage.getItem('resultado'))
+const resultadoLS = getLocalStorage()
 if (resultadoLS) {
     resultado = resultadoLS
 }
@@ -55,13 +54,13 @@ let form = document.getElementById("datos")
 
 
 //----------------Evento aplicado al boton calcular
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
     monto = document.querySelector('#monto').value
     selectCuota = () => {
         let selectorCuotas = document.getElementById("lang")
-        cantCuotas = selectorCuotas.value
-        return
+        cantCuotas = parseInt(selectorCuotas.value)
+
     }
     selectCuota(cantCuotas)
     resultado.push(calculador())
@@ -71,65 +70,19 @@ form.addEventListener("submit", (e) => {
     const card = document.createElement('div')
     card.classList.add('op-list')
 
- 
-    resultado.forEach((eleme) => {
-        card.innerHTML = `
-                            <p>Su prestamo es de: $ ${eleme.monto}</p>
-                            <p>Vas a devolver: $ ${eleme.interes.toFixed(2)}</p>
-                            <p>En ${eleme.cantCuotas} cuotas de $ ${eleme.cuota.toFixed(2)} </p>
-                            <button class="btn btn-success">Solicitar</button>
-                            <button id="borrar" class="btn btn-danger">Borrar</button>
-`
+    resultado.forEach((element) => {
+        card.innerHTML = generateHTML(element)
         prestCalculado.append(card)
     })
-        
+
     //-----------------localStorage
     const calculosJSON = JSON.stringify(resultado)
     localStorage.setItem('resultado', calculosJSON)
-    
-    /*const borrarCard = document.querySelectorAll('.borrar');
-    borrarCard.forEach(buttonDelete => buttonDelete.addEventListener("click", (event) => {
-        event.target.parentElement.remove();
-        // agregar codigo pra eliminar del localStorage por id
-    }));*/
+
+    fnDeleteButtonsEvents();
 })
 
-    const prestCalculado = document.querySelector('#cardCalculos')
-    const mostrarResult = JSON.parse(localStorage.getItem('resultado'))
-    console.log(mostrarResult);
-    
-    mostrarResult.forEach((eleme) => {
-        const card = document.createElement('div')
-        card.classList.add('op-list')
-        card.innerHTML = `
-                                <p>Su prestamo es de: $ ${eleme.monto}</p>
-                                <p>Vas a devolver: $ ${eleme.interes.toFixed(2)}</p>
-                                <p>En ${eleme.cantCuotas} cuotas de $ ${eleme.cuota.toFixed(2)} </p>
-                                <button class="btn btn-success">Solicitar</button>
-                                <button id="borrar" class="btn btn-danger">Borrar</button>
-    `
-        prestCalculado.append(card)
-    })
-    
-    const borrarCard = document.querySelector('#borrar')
-    
-    const fnBorrarCard = () => {
-        const calculo = mostrarResult.find((calcu) => calcu.id === id)
-        const indice =  mostrarResult.indexOf(calculo)
-
-        mostrarResult.splice(indice, 1)
-    }
-
-    borrarCard.addEventListener("click", fnBorrarCard)
-    
-    // -------------borrar elementos
-    // const borrarCard = document.querySelectorAll('.borrar');
-    /*borrarCard.forEach(buttonDelete => buttonDelete.addEventListener("click", (event) => {
-        event.target.parentElement.remove();
-        // agregar codigo pra eliminar del localStorage por id
-    }));*/
-    
-
-
+// Init Functions
+fnAppendLocalStorageElements();
 
 
